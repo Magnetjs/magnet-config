c827fca9cc3740a54036c391f372c5eb89118be2
Kieve Chua <kievechua@gmail.com>
2017-02-14 12:31:42 +0800

    New babel build

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
-import test from 'ava';
-import Config from '../dist/';
+import test from 'ava'
+import Config from '../dist/'
 
 test('Config load basic', async function (t) {
-  let defaultConfig = new Config();
-  await defaultConfig.setup();
+  let defaultConfig = new Config()
+  await defaultConfig.setup()
 
-  t.is(defaultConfig.app.config.app.name, 'magnet-app');
-});
+  t.is(defaultConfig.app.config.app.name, 'magnet-app')
+})
 
 test('Change directories', async function (t) {
-  let changeDirConfig = new Config({}, { paths: ['/config'] });
-  await changeDirConfig.setup();
+  let changeDirConfig = new Config({}, { paths: ['/config'] })
+  await changeDirConfig.setup()
 
-  t.is(changeDirConfig.app.config.app.name, 'test');
-  t.is(changeDirConfig.app.config.es5.test, 'something');
-});
+  t.is(changeDirConfig.app.config.app.name, 'test')
+  t.is(changeDirConfig.app.config.es5.test, 'something')
+})
