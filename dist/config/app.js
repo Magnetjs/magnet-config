b8481a1b76b04ff25b5eddba234df164393d8a81
Kieve Chua <kievechua@gmail.com>
2017-02-15 10:33:26 +0800

    Add test env

--- dist/config/app.js
+++ dist/config/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = {
   env: {
     dev: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' || false,
+    test: !process.env.NODE_ENV || process.env.NODE_ENV === 'test' || false,
     stag: !process.env.NODE_ENV || process.env.NODE_ENV === 'staging' || false,
     prod: process.env.NODE_ENV && process.env.NODE_ENV === 'production' || false
   }
