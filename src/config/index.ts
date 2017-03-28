ee5607423efbfe0104fee3e6a5adbc82990e8696
Kieve Chua <kievechua@gmail.com>
2017-03-28 16:04:56 +0800

    Migrate server to src

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,14 @@
 export default {
   baseDirPath: process.cwd(),
-  configDirPath: '/server/config',
+  configDirPath: '/src/config',
   env: {
     dev: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') || false,
     test: (process.env.NODE_ENV && process.env.NODE_ENV === 'test') || false,
     stag: (process.env.NODE_ENV && process.env.NODE_ENV === 'staging') || false,
-    prod: (process.env.NODE_ENV && process.env.NODE_ENV === 'production') || false
+    prod: (process.env.NODE_ENV && process.env.NODE_ENV === 'production') || false,
+
+    development: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') || false,
+    staging: (process.env.NODE_ENV && process.env.NODE_ENV === 'staging') || false,
+    production: (process.env.NODE_ENV && process.env.NODE_ENV === 'production') || false
   }
 }
